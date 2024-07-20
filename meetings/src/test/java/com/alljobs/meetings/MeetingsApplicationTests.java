01146d32bf38e2278d7c3e20a571537ddf84bc6f
Joao Gilberto Saraiva <jgilbertons@gmail.com>
2024-07-20 19:59:25 -0300

    Add class to SpringBootTest

--- meetings/src/test/java/com/alljobs/meetings/MeetingsApplicationTests.java
+++ meetings/src/test/java/com/alljobs/meetings/MeetingsApplicationTests.java
@@ -3,7 +3,7 @@ package com.alljobs.meetings;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = MeetingsApplication.class)
 class MeetingsApplicationTests {
 
 	@Test
